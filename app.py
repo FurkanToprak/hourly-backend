e0ac574aebc042d5e3a5bf4a6067fcbf8d55adf7
sriram-mittal <sriram.mittal@tamu.edu>
2022-03-07 13:34:41 -0600

    set up tests and linting

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
+"""Main Application File"""
 from flask import Flask
 
 app = Flask(__name__)
-#routes
-from users import routes
+
 
 @app.route("/")
 def hello_world():
+    """Return Hello World"""
     return "<p>Hello, World!</p>"
--- dbConnection.py
+++ dbConnection.py
@@ -1,7 +1,8 @@
 import pymongo
 from pymongo import MongoClient
 
-cluster = MongoClient("mongodb+srv://capstone2022:d2TU1o9kT84Yy3at@cluster0.plj3i.mongodb.net/hourly?retryWrites=true&w=majority")
+cluster = MongoClient(
+    "mongodb+srv://capstone2022:d2TU1o9kT84Yy3at@cluster0.plj3i.mongodb.net/hourly?retryWrites=true&w=majority"
+)
 db = cluster["hourly"]
 collection = db["users"]
-collection.insert_one({"username" : "Sriram", "password" : "1234"})
