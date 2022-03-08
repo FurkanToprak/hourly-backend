ff4d7ebad22a8ffd50702f122323098410ebeac6
Furkan Toprak <furkancemaltoprak@gmail.com>
2022-03-07 23:42:43 -0600

    pre postman

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 """Main Application File"""
-from flask import Flask
+from flask import Flask, request
+from google.oauth2 import id_token
+from google.auth.transport import requests
 
 app = Flask(__name__)
 
@@ -8,3 +10,18 @@ app = Flask(__name__)
 def hello_world():
     """Return Hello World"""
     return "<p>Hello, World!</p>"
+
+@app.route("/google_auth", methods=["POST"])
+def google_auth():
+    """Verifies Google OAuth protocols"""
+    payload = request.json
+    token = payload[token]
+    clientId = payload[clientId]
+    try:
+        idinfo = id_token.verify_oauth2_token(token, requests.Request(), clientId)
+        userid = idinfo['sub']
+        print(userid)
+        return True
+    except ValueError:
+        # Invalid token
+        return False
